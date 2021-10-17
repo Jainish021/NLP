063b615a1e146acfc730169ec39b76528dacd2b0
Jainish Adesara <57753610+Jainish021@users.noreply.github.com>
2021-10-16 19:01:43 -0500

    Update Bag_of_words.py

--- Bag_of_words.py
+++ Bag_of_words.py
@@ -8,8 +8,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 vocabulary = vocab()
 
 # Defining the list of path where all the training data files are stored.
-filePaths = (list(paths.list_files("enron1/train/ham")) + list(paths.list_files("enron4/train/ham")) + list(paths.list_files("hw1/train/ham"))
-             + list(paths.list_files("enron1/train/spam")) + list(paths.list_files("enron4/train/spam")) + list(paths.list_files("hw1/train/spam")))
+filePaths = #Enter the path to files
 
 # Reading all training data file and preparing a corresponding features*example matrix for bag_of_words model using CountVectorizer() method.
 vec = CountVectorizer()
@@ -34,84 +33,5 @@ def BOW():
     return mat.toarray(), df
 
 
-def BOW_test(filePaths):
-    # Initializing the data list
-    data = []
-    true_y = []
-
-    for filePath in filePaths:
-        with open(filePath, errors="ignore") as f:
-            contents = f.read()
-            data.append(contents)
-            if "ham" in filePath:
-                true_y.append(0)
-            else:
-                true_y.append(1)
-            f.close()
-    vec.fit(vocabulary)
-    mat = vec.transform(data)
-    return mat.toarray(), true_y
-
-
-# Defining a ham function to create a features*example matrix of ham training data.
-def ham():
-    # Defining the list of path where all the ham training data files are stored.
-    filePaths = (list(paths.list_files("enron1/train/ham")) + list(
-        paths.list_files("enron4/train/ham")) + list(paths.list_files("hw1/train/ham")))
-
-    data = []
-
-    # Reading all training data file and preparing a corresponding features*example matrix for bag_of_words model using CountVectorizer() method.
-    for filePath in filePaths:
-        with open(filePath, errors="ignore") as f:
-            contents = f.read()
-            data.append(contents)
-            f.close()
-    vec.fit(vocabulary)
-    mat = vec.transform(data)
-
-    cols = vec.get_feature_names()
-    rows = range(len(mat.toarray()))
-    df = pd.DataFrame(mat.toarray(), columns=cols, index=rows)
-    return mat.toarray(), df
-
-
-# Defining a spam function to create a features*example matrix of spam training data.
-def spam():
-    # Defining the list of path where all the spam training data files are stored.
-    filePaths = (list(paths.list_files("enron1/train/spam")) + list(
-        paths.list_files("enron4/train/spam")) + list(paths.list_files("hw1/train/spam")))
-
-    data = []
-
-    # Reading all training data file and preparing a corresponding features*example matrix for bag_of_words model using CountVectorizer() method.
-    for filePath in filePaths:
-        with open(filePath, errors="ignore") as f:
-            contents = f.read()
-            data.append(contents)
-            f.close()
-    vec.fit(vocabulary)
-    mat = vec.transform(data)
-
-    cols = vec.get_feature_names()
-    rows = range(len(mat.toarray()))
-    df = pd.DataFrame(mat.toarray(), columns=cols, index=rows)
-    return mat.toarray(), df
-
-
-# Creating a vector of given classes in the training dataset.
-def y():
-    class_data = []
-    for filePath in filePaths:
-        if "ham" in filePath:
-            class_data.append(0)
-        else:
-            class_data.append(1)
-    return class_data
-
-
 if __name__ == "__main__":
-    y = y()
-    h = ham()
-    s = spam()
     bow = BOW()
