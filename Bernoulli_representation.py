e824d56fa6e92bdd1416deec5d294e71fe9b8762
Jainish Adesara <57753610+Jainish021@users.noreply.github.com>
2021-10-16 19:09:02 -0500

    Update and rename Bernoulli.py to Bernoulli_representation.py

--- Bernoulli_representation.py
+++ Bernoulli_representation.py
@@ -1,40 +1,26 @@
-from Vocabulary import vocab
-from Bag_of_words import ham, spam
+#Importing functions from other file.
+from AlphaNumeric_vocabulary_builder import vocab
+
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 
 
-# Importing the vocabulary built in Vocabulary.py file
+# Importing the vocabulary built in Vocabulary.py file.
 vocabulary = vocab()
 
 # Reading all training data file and preparing a corresponding features*example matrix for bag_of_words model using CountVectorizer() method.
 vec = CountVectorizer()
 
+filePaths = #Enter the path to text files.
 
-# Converting the dataframe created in the bag of words module to the matrix of bernoulli model using masking method.
+# Preproccesing the data and converting it into bernoulli model data.
 def bernoulli():
-    _, ham_data = ham()
-    _, spam_data = spam()
-
-    ham_data = ham_data.mask(ham_data > 0, 1)
-    spam_data = spam_data.mask(ham_data > 0, 1)
-
-    return ham_data.to_numpy(), spam_data.to_numpy()
-
-
-# Preproccesing the test data and converting it into bernoulli model data.
-def bernoulli_test(filePaths):
     data = []
-    true_y = []
 
     for filePath in filePaths:
         with open(filePath, errors="ignore") as f:
             contents = f.read()
             data.append(contents)
-            if "ham" in filePath:
-                true_y.append(0)
-            else:
-                true_y.append(1)
             f.close()
     vec.fit(vocabulary)
     mat = vec.transform(data)
@@ -45,9 +31,9 @@ def bernoulli_test(filePaths):
 
     test_data = df.mask(df > 0, 1)
 
-    return test_data.to_numpy(), true_y
+    return test_data.to_numpy()
 
 
 if __name__ == "__main__":
     ber = bernoulli()
-    ber_test = bernoulli_test
+    print(ber)
